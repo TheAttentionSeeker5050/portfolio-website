4f7ce7200042ba94b5b3f0869bf2af0e3b8fc434
nicolas castellano <nicolas.castellano.6@gmail.com>
2023-03-11 22:28:49 -0400

    created portfolio page

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import "./../sass/portfolio.scss"
 import "./../sass/styles.scss"
 import "./../sass/main.scss"
 
+import "./../node_modules/@fortawesome/fontawesome-free/js/fontawesome";
+import "./../node_modules/@fortawesome/fontawesome-free/js/regular";
+import "./../node_modules/@fortawesome/fontawesome-free/js/brands";
+import "./../node_modules/@fortawesome/fontawesome-free/js/solid";
 
 // // import scripts
 // import {codeAnimation} from './scripts/code_animation.js';
